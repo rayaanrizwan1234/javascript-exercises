5e01afad7196b90e46fb4a65b89cbe4acd8dcd3d
Rayaan Rizwan <omgitzray23@gmail.com>
2023-06-24 18:14:59 +0400

    Finished 1 and 2 ex

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    let res = "";
+    for (let i = 0; i <n; i++){
+        res += str;
+    }
+    if (n < 0){
+        return "ERROR";
+    }
+    return res;
 };
 
 // Do not edit below this line
