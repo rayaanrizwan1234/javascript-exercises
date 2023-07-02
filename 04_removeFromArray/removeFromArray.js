af9a47f1aad4f23c92fb78dc5055ace971b791b4
Rayaan Rizwan <omgitzray23@gmail.com>
2023-07-03 01:21:25 +0400

    Finished 3, 4, 5 ex

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    // first need to fine the index of the element you want to remove
+    // then use splice 
+    let arr = arguments[0];
 
+    for (let i = 1; i < arguments.length; i++){
+        let index = arr.indexOf(arguments[i]);
+        if (index != - 1){
+            arr.splice(index, 1);
+        }
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if ((!Number.isInteger(x)) || (!Number.isInteger(y)) ||y < 0 || x < 0){
+        return "ERROR"
+    }
+    if (y < x){
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+    for (let i = x; i <= y; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
