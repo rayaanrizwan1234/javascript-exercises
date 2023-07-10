7a1953ba38f03cc6841cc3dd68a39769c7d9c2af
Rayaan Rizwan <omgitzray23@gmail.com>
2023-07-10 17:29:48 +0400

    Finished more exs

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+function century(number){
+    const numS = number.toString();
+    const last = numS.slice(-2);
+    if (last == "00"){
+        return true
+    }
+    return false
+}
 
+const leapYears = function(year) {
+    if (century(year)){
+        if (year % 400 == 0){
+            return true;
+        }
+        return false
+    }
+
+    if (year % 4 == 0){
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
